71dccbda56f14d7a415ad761737ad9b019f223bb
Yash Kulshrestha <kulshres@amazon.com>
2022-10-03 13:10:44 -0700

    changed the cloudwatch logs agent config into just a simple object

--- src/CloudwatchAgentCfnInitConfig.ts
+++ src/CloudwatchAgentCfnInitConfig.ts
@@ -25,31 +25,27 @@ export default function buildCloudWatchCfnInitConfig(logGroupName: string) {
 }
 
 function getCwAgentConfigForLogGroup(logGroupName: string) {
-  return JSON.stringify(
-    {
-      agent: {
-        debug: true,
-      },
-      logs: {
-        logs_collected: {
-          files: {
-            collect_list: [
-              {
-                file_path: '/var/log/ecs/ecs-agent.log',
-                log_group_name: logGroupName,
-                log_stream_name: '{instance_id}/agent-logs',
-              },
-              {
-                file_path: '/var/log/ecs/ecs-init.log',
-                log_group_name: logGroupName,
-                log_stream_name: '{instance_id}/init-logs',
-              },
-            ],
-          },
+  return {
+    agent: {
+      debug: true,
+    },
+    logs: {
+      logs_collected: {
+        files: {
+          collect_list: [
+            {
+              file_path: '/var/log/ecs/ecs-agent.log',
+              log_group_name: logGroupName,
+              log_stream_name: '{instance_id}/agent-logs',
+            },
+            {
+              file_path: '/var/log/ecs/ecs-init.log',
+              log_group_name: logGroupName,
+              log_stream_name: '{instance_id}/init-logs',
+            },
+          ],
         },
       },
     },
-    null,
-    2
-  );
+  };
 }
