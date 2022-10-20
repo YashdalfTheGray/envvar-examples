0263bc76b4a990f5e06100ead04c76d378df5e58
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-10-19 23:45:12 -0400

    added the appropriate permissions to the logs only role

--- src/CloudwatchLogsOnlyRole.ts
+++ src/CloudwatchLogsOnlyRole.ts
@@ -1,11 +1,29 @@
 import { Construct } from 'constructs';
-import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import {
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from 'aws-cdk-lib/aws-iam';
 
 export default class CloudwatchLogsOnlyRole extends Role {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
       roleName: 'cloudwatchLogsOnlyInstanceRole',
+    });
+
+    const logsWritePolicy = new PolicyDocument({
+      statements: [
+        new PolicyStatement({
+          resources: ['*'],
+          actions: ['logs:CreateLogStream', 'logs:PutLogEvents'],
+        }),
+      ],
+    });
+
+    this.attachInlinePolicy({
+      logsWritePolicy,
     });
   }
 }
